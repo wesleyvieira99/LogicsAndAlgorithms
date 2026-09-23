173926eec77f32bd6ccbc7c4a85868fcb2d8c0d2
Wesley Vieira <wesleysv11@gmail.com>
2026-09-23 06:34:40 -0300

    feat: implementando logica do problema de Sliding Window

--- src/main/java/org/example/MaximumAverageSubarray.java
+++ src/main/java/org/example/MaximumAverageSubarray.java
@@ -10,6 +10,29 @@ Find a contiguous subarray whose length is equal to k that has the maximum avera
 
 public class MaximumAverageSubarray {
 
+    // Step 0: the method creation
+    public double findMaxAvg(int[] nums, int k) {
 
+        // Step 1: Calculate the sum of the initial window of size k, the first loop with the first window
+        double currentSum = 0;
+        for(int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
+
+        // Step 2: the initial window end and, at this moment, we have the initial maxSum
+        double maxSum = currentSum;
+
+        // Step 3: Slide the window from index k to the end of the array
+        for(int i = k; i < nums.length; i++) {
+            //Add the new element entering the window , subtract the one leaving
+            //With this manner, we are going each step to an item to the left
+            currentSum += nums[i] - nums[i - k];
+
+            //Update the maximum sum found so far
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum / k;
+    }
 
 }
